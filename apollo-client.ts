2a28ed7e7f95d1c42d534e47a6e59816a39de658
Noah Milberger <n.milberger@gmail.com>
2023-02-14 13:46:43 -0500

    Updated apollo-client to point to deployed postgres.

--- apollo-client.ts
+++ apollo-client.ts
@@ -2,7 +2,7 @@ import { ApolloClient, createHttpLink, InMemoryCache } from "@apollo/client";
 import { setContext } from "@apollo/client/link/context";
 
 const httpLink = createHttpLink({
-  uri: "http://localhost:4000/api",
+  uri: `${process.env.NEXT_PUBLIC_BACKEND_URI}/api`,
 });
 
 const sessionLink = setContext((_, { headers }) => {
