048e4ead7ecaa1e9ef5d9d26c56061dd4ac4010f
Noah Milberger <n.milberger@gmail.com>
2023-02-15 19:22:48 -0500

    Added posthog.

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,10 +1,34 @@
 import "@/styles/globals.css";
+import { useRouter } from "next/router";
+import { useEffect } from "react";
 import type { AppProps } from "next/app";
+import posthog from "posthog-js";
 import { ApolloProvider } from "@apollo/client";
 import { CookiesProvider } from "react-cookie";
 import client from "../../apollo-client";
 
+if (typeof window !== "undefined" && process.env.NEXT_PUBLIC_POSTHOG_API_KEY) {
+  posthog.init(process.env.NEXT_PUBLIC_POSTHOG_API_KEY, {
+    api_host: "https://app.posthog.com",
+    loaded: (posthog) => {
+      if (process.env.NODE_ENV === "development") posthog.opt_out_capturing();
+    },
+  });
+}
+
 export default function App({ Component, pageProps }: AppProps) {
+  const router = useRouter();
+
+  useEffect(() => {
+    // Track page views
+    const handleRouteChange = () => posthog.capture("$pageview");
+    router.events.on("routeChangeComplete", handleRouteChange);
+
+    return () => {
+      router.events.off("routeChangeComplete", handleRouteChange);
+    };
+  }, []);
+
   return (
     <ApolloProvider client={client}>
       <CookiesProvider>
