0df525e92e1810a4e7ea7f6d696684a752bba018
Noah Milberger <n.milberger@gmail.com>
2023-04-21 21:36:37 -0400

    Fixed bug with routing between pages.

--- src/components/ui/page/Page.tsx
+++ src/components/ui/page/Page.tsx
@@ -7,24 +7,12 @@ interface PageProps {
 }
 
 export default function Page({ children }: PageProps) {
-  const size = useWindowSize();
-  const [height, setHeight] = useState(0);
-
-  useEffect(() => {
-    setHeight(size.height);
-  }, [size]);
-
   return (
-    <div
-      className="flex flex-col bg-main-dark text-main-dark w-full"
-      style={{
-        height,
-      }}
-    >
+    <div className="flex flex-col bg-main-dark text-main-dark w-full h-screen">
       <div className="flex h-28">
         <TopBar />
       </div>
-      <div className="flex w-full h-full overflow-auto">{children}</div>
+      <div className="flex flex-1 w-full h-full overflow-auto">{children}</div>
     </div>
   );
 }
